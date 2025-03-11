110bfe6787180bb4a7f3efb8466b44718cb3a82c
haiun <gewdox@naver.com>
2025-03-11 23:49:47 +0900

    commit

--- Assets/Scripts/OutlineMeshMaker.cs
+++ Assets/Scripts/OutlineMeshMaker.cs
@@ -11,54 +11,29 @@ public class OutlineMeshMaker : MonoBehaviour
     
     void Awake()
     {
-        if (false)
+        var renderers = transform.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var r in renderers)
         {
-            var renderers = transform.GetComponentsInChildren<MeshRenderer>();
-            foreach (var r in renderers)
+            if (r.material.IsKeywordEnabled("_ALPHABLEND_ON"))
+                continue;
+
+            var go = new GameObject(r.gameObject.name + "_Outline");
+            go.transform.SetParent(r.transform, false);
+            
+            var outlineMeshRenderer = go.AddComponent<SkinnedMeshRenderer>();
+            outlineMeshRenderer.sharedMesh = r.sharedMesh;
+            outlineMeshRenderer.localBounds = r.localBounds;
+            outlineMeshRenderer.rootBone = r.rootBone;
+            outlineMeshRenderer.bones = r.bones.ToArray();
+            for (int i = 0; i < r.sharedMesh.blendShapeCount; ++i)
             {
-                if (r.material.IsKeywordEnabled("_ALPHABLEND_ON"))
-                    continue;
-
-                var meshFilter = r.gameObject.GetComponent<MeshFilter>();
-                
-                var go = new GameObject(r.gameObject.name + "_Outline");
-                go.transform.SetParent(r.transform, false);
-                
-                var outlineMeshFilter = go.AddComponent<MeshFilter>();
-                outlineMeshFilter.mesh = meshFilter.mesh;
-                
-                var outlineMeshRenderer = go.AddComponent<MeshRenderer>();
-                var outlineMaterial = new Material(_outlineShader);
-                outlineMeshRenderer.material = outlineMaterial;
-                outlineMaterial.mainTexture = r.material.mainTexture;
+                outlineMeshRenderer.SetBlendShapeWeight(i, r.GetBlendShapeWeight(i));
             }
-        }
-
-        {
-            var renderers = transform.GetComponentsInChildren<SkinnedMeshRenderer>();
-            foreach (var r in renderers)
-            {
-                if (r.material.IsKeywordEnabled("_ALPHABLEND_ON"))
-                    continue;
-
-                var go = new GameObject(r.gameObject.name + "_Outline");
-                go.transform.SetParent(r.transform, false);
-                
-                var outlineMeshRenderer = go.AddComponent<SkinnedMeshRenderer>();
-                outlineMeshRenderer.sharedMesh = r.sharedMesh;
-                outlineMeshRenderer.localBounds = r.localBounds;
-                outlineMeshRenderer.rootBone = r.rootBone;
-                outlineMeshRenderer.bones = r.bones.ToArray();
-                for (int i = 0; i < r.sharedMesh.blendShapeCount; ++i)
-                {
-                    outlineMeshRenderer.SetBlendShapeWeight(i, r.GetBlendShapeWeight(i));
-                }
-                
-                var outlineMaterial = new Material(_outlineShader);
-                outlineMeshRenderer.material = outlineMaterial;
-                outlineMaterial.mainTexture = r.material.mainTexture;
-                outlineMaterial.SetFloat("_Thickness", _outlineThickness);
-            }
+            
+            var outlineMaterial = new Material(_outlineShader);
+            outlineMeshRenderer.material = outlineMaterial;
+            outlineMaterial.mainTexture = r.material.mainTexture;
+            outlineMaterial.SetFloat("_Thickness", _outlineThickness);
         }
     }
 }
